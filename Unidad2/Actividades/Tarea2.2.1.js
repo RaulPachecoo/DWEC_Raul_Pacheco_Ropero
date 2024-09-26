8913a645d1b26babcb248cccd36fd1aa8ff89772
RaulPachecoo <raulpachecoropero@gmail.com>
2024-09-26 12:14:29 +0200

    Creación de la tarea 2.2.1

---
+++ Unidad2/Actividades/Tarea2.2.1.js
@@ -0,0 +1,38 @@
+// 1- No matter if you don’t understand the following code, rewrite it following good practices
+
+function pow(x,n)
+{
+ let result=1;
+ for(let i=0;i<n;i++) {result*=x;}
+ return result;
+}
+let x=prompt("x?",''), n=prompt("n?",'')
+if (n<=0)
+{
+ alert(`Power ${n} is not supported, please enter an integer number greater
+than zero`);
+}
+else
+{
+ alert(pow(x,n))
+}
+
+/*
+function pow(x, n) {
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+      result *= x;
+    }
+    return result;
+  }
+  
+  let x = +prompt("x?", ''); // Convertir x a número
+  let n = +prompt("n?", ''); // Convertir n a número
+  
+  if (n <= 0) {
+    alert(`Power ${n} is not supported, please enter an integer number greater than zero`);
+  } else {
+    alert(pow(x, n));
+  }
+
+*/
